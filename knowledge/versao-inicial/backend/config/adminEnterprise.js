38edce875c327f064bd2dd67fa2fe5a1dcf56542
Joan Pedro <jpos.senai@gmail.com>
2020-02-13 16:11:47 -0300

    Removing

--- knowledge/versao-inicial/backend/config/adminEnterprise.js
+++
@@ -1,9 +0,0 @@
-module.exports = middleware_1 => {
-    return (req, res, next) => {
-        if (req.user.adminEnterprise) {
-            middleware_1(req, res, next)
-        } else {
-            res.status(401).send('Usuário não possui nível de acesso suficiente! [Admin Enterprise]')
-        }
-    }
-}
--- knowledge/versao-inicial/backend/config/manager.js
+++
@@ -1,9 +0,0 @@
-module.exports = middleware_2 => {
-    return (req, res, next) => {
-        if (req.user.manager) {
-            middleware_2(req, res, next)
-        } else {
-            res.status(401).send('Usuário não possui nível de acesso suficiente! [Manager]')
-        }
-    }
-}
