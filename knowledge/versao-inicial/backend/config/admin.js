70ead8a5ad9b9d94fc28954f79cd89e1c0cb10be
Joan Pedro <jpos.senai@gmail.com>
2020-02-13 15:49:04 -0300

    Knowledge removido

--- knowledge/versao-inicial/backend/config/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = middleware => {
-    return (req, res, next) => {
-        if (req.user.adminMaster) {
-            middleware(req, res, next)
-        } else {
-            res.status(401).send('Usuário não possui nível de acesso suficiente! [Admin Master]')
-        }
-    }
-}
