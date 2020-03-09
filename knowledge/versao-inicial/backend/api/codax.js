6889a293a26869cdda3fad7638d366abfc38ed1d
Joan Pedro <jpos.senai@gmail.com>
2020-03-09 16:26:31 -0300

    Adiciona Alerta de Resultados da Rota Codax

--- knowledge/versao-inicial/backend/api/codax.js
+++ knowledge/versao-inicial/backend/api/codax.js
@@ -2,39 +2,42 @@ var mysql = require('mysql');
 
 module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
-
-    const save = (req, res) => {
-    const codax = {...req.body }
-    if (req.params.id) circuitId.id = req.params.id
-
-    try {
-        existsOrError(codax.hourStamp, 'Hora não informada.')
-        existsOrError(codax.dateStamp, 'Data não informada.')
-        existsOrError(codax.value, 'Valor não informado.')
-        existsOrError(codax.unit, 'Unidade não informada.')
-        existsOrError(codax.circuitId, 'Id do circuito não informado.')
-    } catch (msg) {
-        res.status(400).send(msg)
-    }
-
     const con = mysql.createConnection({
         host: "codax.c8rmjbl8lqg6.us-west-2.rds.amazonaws.com",
         user: "codax",
         password: "codax2020",
         database: "innodb"
     })
-      
-    var sql = "INSERT INTO mydb.Tbl_medicoes(data, hora, valor, unidade, Tbl_circuitos_idCircuito) VALUES ('"+codax.dateStamp+"', '"+codax.hourStamp+"', '"+codax.value+"', '"+codax.unit+"', '"+codax.circuitId+"');"
     
-    con.connect(e => {
-        if (e) throw e;
-        console.log("Connected!");
-        con.query(sql, (e, result) => {
+    const save = (req, res) => {
+        const codax = {...req.body }
+        if (req.params.id) circuitId.id = req.params.id
+
+        try {
+            existsOrError(codax.hourStamp, 'Hora não informada.')
+            existsOrError(codax.dateStamp, 'Data não informada.')
+            existsOrError(codax.value, 'Valor não informado.')
+            existsOrError(codax.unit, 'Unidade não informada.')
+            existsOrError(codax.circuitId, 'Id do circuito não informado.')
+        } catch (msg) {
+            res.status(400).send(msg)
+        }
+        
+        var sql = "INSERT INTO mydb.Tbl_medicoes(data, hora, valor, unidade, Tbl_circuitos_idCircuito) VALUES ('"+codax.dateStamp+"', '"+codax.hourStamp+"', '"+codax.value+"', '"+codax.unit+"', '"+codax.circuitId+"');"
+        
+        con.connect(e => {
             if (e) throw e;
-            console.log(`1 record inserted: ${result}`)
-        })
-    });
-    res.send(codax)   
+            console.log("Conexão com o MySQL realizada.");
+            con.query(sql, (e, result) => {
+                if (e) throw e;
+                //console.log(result)
+                console.log("Número de linhas afetadas: " + result.affectedRows);
+                console.log("Número de gravações afetadas com avisos: " + result.warningCount);
+                console.log("Menssagem do MySQL Server: " + result.message);
+            })
+        });
+
+        res.send(codax)   
     }
     
     return { save } 
